0b178eea298165008661cf18d0bc7634484220eb
casey <leptone55@gmail.com>
2017-04-07 21:21:09 -0700

    added LayerControls

--- src/Map.js
+++ src/Map.js
@@ -1,55 +1,56 @@
 import React from 'react';
 //import { render } from 'react-dom';
-import { Map, Polyline, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Map, Polyline, Marker, Popup, TileLayer, LayersControl } from 'react-leaflet';
+
+const { BaseLayer, Overlay } = LayersControl
+
+
+import ToggleContainer from './ToggleContainer'
+
+console.log('LayersControl', LayersControl)
+console.log('BaseLayer', BaseLayer)
 
 import parser from './parser'
 
 import route1 from './data/route1'
 import route2 from './data/route2'
+import route3 from './data/route3'
 
 import midPointGen from './midPointFinder'
 import genPolyline from './genPolyline'
 
+//console.log('route2', route2)
+//console.log('route3', route3)
+
 let parsed1 = parser(route1)
 let parsed2 = parser(route2)
+let parsed3 = parser(route3)
 
-//console.log('1', parser1)
-//console.log('2', parser2)
+let position =  midPointGen([ parsed1, parsed2, parsed3 ])
 
-let position =  midPointGen([ parsed1, parsed2 ])
+//console.log('position', position)
 
-console.log('position', position)
-/*
-const polyline = [
-    [37.775,	-122.401],
-    [37.7749,	-122.401],
-    [37.7742,	-122.4],
-    [37.7726,	-122.399],
-    [37.7716,	-122.398],
-    [37.7712,	-122.398],
-    [37.771,	-122.398],
-    [37.7709,	-122.398]
-]
-const multiPolyline = [
-    [[51.5, -0.1], [51.5, -0.12], [51.52, -0.12]],
-    [[51.5, -0.05], [51.5, -0.06], [51.52, -0.06]]
-]
-*/
 const multiPolyline = [
     genPolyline(parsed1),
     genPolyline(parsed2),
+    genPolyline(parsed3)
 ]
-console.log('multiPolyline', multiPolyline)
 
+console.log('multiline', multiPolyline)
 export default class MapContainer extends React.Component {
     render() {
         return (
-            <Map center={position} zoom={13}>
-                <TileLayer
-                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-                <Polyline color='red' positions={multiPolyline} />
+            <Map center={position} zoom={15}>
+				<Polyline color='red' positions={multiPolyline} />
+                <LayersControl position='topright'>
+					<BaseLayer checked name='OpenStreetMap.Mapnik'>
+					<TileLayer
+					  attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					  url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+					/>
+				  </BaseLayer>
+
+                </LayersControl>
 
             </Map>
         )
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import Map from './Map'
+//import Map from './Map'
 
 ReactDOM.render(
-  <Map />,
+  <App/>,
   document.getElementById('root')
 );
